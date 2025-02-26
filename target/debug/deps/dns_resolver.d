e90ee40ddca470f224821814be98779320b5dba4
knzr133 <insraze@gmail.com>
2025-02-26 15:42:53 +0200

    Add files via upload

---
+++ target/debug/deps/dns_resolver.d
@@ -0,0 +1,5 @@
+C:\Users\insra\RustroverProjects\untitled1\dns_resolver\target\debug\deps\dns_resolver.exe: src\main.rs
+
+C:\Users\insra\RustroverProjects\untitled1\dns_resolver\target\debug\deps\dns_resolver.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/dns_resolver.d
@@ -0,0 +1,1 @@
+C:\Users\insra\RustroverProjects\untitled1\dns_resolver\target\debug\dns_resolver.exe: C:\Users\insra\RustroverProjects\untitled1\dns_resolver\src\main.rs
